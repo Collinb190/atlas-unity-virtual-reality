e8fb7074f0dfa604224b2365d3f2254664952bb1
Lin <100497279+Collinb190@users.noreply.github.com>
2024-09-24 09:56:16 -0500

    here we go

--- 360-video/Assets/scripts/VideoManager360.cs
+++ 360-video/Assets/scripts/VideoManager360.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 
 public class VideoManager360 : MonoBehaviour
@@ -11,6 +12,7 @@ public class VideoManager360 : MonoBehaviour
     public Material videoMaterial;
     public VideoPlayer videoPlayer;
     public float fadeDuration = 1f;
+    public float rewindFastForwardTime = 10f; // Amount of time to rewind/fast-forward (in seconds)
 
     private Material _skyMaterial;
 
@@ -18,6 +20,77 @@ public class VideoManager360 : MonoBehaviour
     void Start()
     {
         _skyMaterial = RenderSettings.skybox;
+    }
+
+    public void LoadMainMenu()
+    {
+        // Load the corresponding level scene
+        SceneManager.LoadScene("Main");
+    }
+
+    public void LoadCantina()
+    {
+        // Load the corresponding level scene
+        SceneManager.LoadScene("Cantina");
+    }
+
+    public void LoadCube()
+    {
+        // Load the corresponding level scene
+        SceneManager.LoadScene("Cube");
+    }
+
+    public void LoadLivingRoom()
+    {
+        // Load the corresponding level scene
+        SceneManager.LoadScene("LivingRoom");
+    }
+
+    public void LoadMezzanine()
+    {
+        // Load the corresponding level scene
+        SceneManager.LoadScene("Mezzanine");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exited");
+        Application.Quit();
+    }
+
+    // Adjust playback speed half
+    public void SetPlaybackSpeedNormal()
+    {
+        videoPlayer.playbackSpeed = 1.0f;
+    }
+
+    // Adjust playback speed normal
+    public void SetPlaybackSpeedHalf()
+    {
+        videoPlayer.playbackSpeed = 0.5f;
+    }
+
+    // Adjust playback speed double
+    public void SetPlaybackSpeedDouble()
+    {
+        videoPlayer.playbackSpeed = 2.0f;
+    }
+
+    public void ToggleLooping()
+    {
+        videoPlayer.isLooping = true; // Enable looping when this button is pressed
+    }
+
+    // Rewind video by a specific amount of time
+    public void Rewind()
+    {
+        videoPlayer.time = Math.Max(videoPlayer.time - rewindFastForwardTime, 0);
+    }
+
+    // Fast-forward video by a specific amount of time
+    public void FastForward()
+    {
+        videoPlayer.time = Math.Min(videoPlayer.time + rewindFastForwardTime, videoPlayer.length);
     }
 
     public void StartVideo()
